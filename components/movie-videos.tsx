d3bb22ec2c014593e687d1cf641362d533e1a07c
Ralto13 <doorumi13@gmail.com>
2024-11-15 18:36:29 +0900

    prefetch

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,8 +2,6 @@ import { API_URL } from "../app/constant";
 import styles from "../styles/movie-videos.module.css"
 
 async function getVideos(id: string) {
-  console.log(`fetching videos`);
-  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
